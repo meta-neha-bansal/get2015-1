490aa9017ae73c124b1a9015d376fa83be762058
meta-neha-bansal <neha.bansal@metacube.com>
2015-08-11 14:11:05 +0530

    Update PF_assignment2_Q1.java

--- PF/assignment2/PF_assignment2_Q1.java
+++ PF/assignment2/PF_assignment2_Q1.java
@@ -1,29 +1,39 @@
-
+//This program is mainly for displaying these type of pattern.
+/*
+      1
+     121
+    12321
+   1234321
+    12321
+     121
+      1
+*/
 public class PF_assignment2_Q1 {
-	public String pyramid(int n)
+	public String pyramid(int row)
 	{
 		int i=0;
 		String finalString="";
 		String str1="",str2="";
-		for(i=1;i<=n;i++)
+		//below code is for upper half of pattern
+		for(i=1;i<=row;i++)
 		{
-			str1=spaces(i,n);
-			str2=numbers(i,n);
-			finalString+=str1+str2+"\n";
+			str1=spaces(i,n);                //calling function of space for each row
+			str2=numbers(i,n);              //calling function of numbers each row
+			finalString+=str1+str2+"\n";      
 		}
-		for(i=n-1;i>0;i--)
+		//below code is for lower half of pattern
+		for(i=row-1;i>0;i--)
 		{
 			str1=spaces(i,n);
 			str2=numbers(i,n);
-			
 			finalString+=str1+str2;
 			if(i!=1)
 				finalString+="\n";
 		}
-		
-		return finalString;
+		return finalString;                //return the final string of this pattern
 		
 	}
+	//below function is to return string of spaces.
 	public String spaces(int row,int n)
 	{
 		String str="";
@@ -33,6 +43,7 @@ public class PF_assignment2_Q1 {
 		return str;	
 		
 	}
+	//below function is to return string of numbers.
 	public String numbers(int row,int n)
 	{
 		String str="";
